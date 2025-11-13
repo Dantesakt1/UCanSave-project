45ac3466b52485e03fc98496c2853051b265ba9c
elpepesech12 <fe.lopezl@duocuc.cl>
2025-11-13 16:41:18 -0300

    Se agrega pagina de compra

---
+++ src/pages/PagoExitoso.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PagoExitoso() {
+    return (
+        <main>
+            <div className="resultado-pago-container">
+                <div className="icono-exito">
+                    ✓ {/* Símbolo de check */}
+                </div>
+                <h1 style={{ color: '#44b699' }}>¡Pago Exitoso!</h1>
+                <p>Tu apadrinamiento ha sido procesado correctamente. Hemos enviado un comprobante a tu correo.</p>
+                <p>¡Gracias por ayudarnos a salvar especies!</p>
+                <Link to="/" className="btn-volver">Volver al Inicio</Link>
+            </div>
+        </main>
+    );
+}
+
+export default PagoExitoso;
